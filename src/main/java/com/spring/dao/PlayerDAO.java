607994130caca77beb118b5a79dce1586b45c181
Hassan Askar <hassanemad15697@gmail.com>
2022-02-06 17:18:23 +0100

    woking with starter data rest to autoGenerate the API without controller and service layers

--- src/main/java/com/spring/dao/PlayerDAO.java
+++ src/main/java/com/spring/dao/PlayerDAO.java
@@ -3,11 +3,8 @@ package com.spring.dao;
 import com.spring.model.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface PlayerDAO extends JpaRepository<Player,Integer> {
-//	public List<Player> getAllPlayers();
-//	public void addPlayer(Player player);
-//	public Player getPlayer(int id);
-//	public int deletePlayer(int id);
+    /*
+    http://localhost:8080/players
+     */
 }
